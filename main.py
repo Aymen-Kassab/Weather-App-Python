86b59c2c3a9fb6e004d4dc28fb90eb25e32c8cb9
Aymen-Kassab <aymankassab123@gmail.com>
2025-09-09 00:00:14 +0100

    Setting the application

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-class MainWindow:
-    pass
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Weather App")
+        self.setWindowIcon(QIcon("logo.png"))
 
 def main():
-    pass
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
