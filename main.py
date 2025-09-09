612bfb67c4ad91f1d591724325961196ef5530aa
Aymen-Kassab <aymankassab123@gmail.com>
2025-09-09 22:40:28 +0100

    Adding styles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 
@@ -10,13 +10,20 @@ class MainWindow(QWidget):
         self.setWindowIcon(QIcon("logo.png"))
         self.setGeometry(700, 300, 500, 600)
         self.text_label = QLabel("Enter city name:", self)
+        self.input_label = QLineEdit(self)
         self.search_btn = QPushButton("Search", self)
         self.weather_label = QLabel("35 C", self)
         self.emote_label = QLabel("🌞", self)
-        self.description_label = QLabel("Sunny", self)
+        self.description_label = QLabel("The weather is sunny", self)
         self.initUI()
 
     def initUI(self):
+        # Giving names to objects
+        self.text_label.setObjectName("text_label")
+        self.search_btn.setObjectName("search_btn")
+        self.weather_label.setObjectName("weather_label")
+        self.emote_label.setObjectName("emote_label")
+        self.description_label.setObjectName("description_label")
         # Setting text labels at the center of the window
         self.text_label.setAlignment(Qt.AlignCenter)
         self.weather_label.setAlignment(Qt.AlignCenter)
@@ -26,17 +33,31 @@ class MainWindow(QWidget):
         vbox = QVBoxLayout()
         # Adding labels to the layout
         vbox.addWidget(self.text_label)
+        vbox.addWidget(self.input_label)
         vbox.addWidget(self.search_btn)
         vbox.addWidget(self.weather_label)
         vbox.addWidget(self.emote_label)
         vbox.addWidget(self.description_label)
         self.setLayout(vbox)
         # App Styling
-        self.setStyleSheet("""
-        
-        """)
-
-
+        self.text_label.setStyleSheet("font-size: 35px;"
+                                      "font-weight: bold;")
+        self.input_label.setStyleSheet("background-color: #fafafa;"
+                                       "border: 2px solid #000000;"
+                                       "height: 50px;"
+                                       "border-radius: 15px;"
+                                       "font-size: 30px;"
+                                       "padding-left: 20px;"
+                                       "margin: 0 70px;")
+        self.search_btn.setStyleSheet("font-size: 35px;"
+                                      "font-weight: bold;"
+                                      "margin: 0 70px;")
+        self.weather_label.setStyleSheet("font-size: 70px;"
+                                         "font-weight: bold;")
+        self.emote_label.setStyleSheet("font-size: 70px;"
+                                         "font-weight: bold;")
+        self.description_label.setStyleSheet("font-size: 30px;"
+                                         "font-weight: bold;")
 
 def main():
     app = QApplication(sys.argv)
